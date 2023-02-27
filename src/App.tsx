139fc7639526ee2b490c54ed44d1b9290eb03977
vietthom <thomle0418@gmail.com>
2023-02-26 23:23:07 -0700

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from 'react';
+import { SelectedPage } from "@/shared/types";
+import { useEffect } from 'react';
+
+
+
+
 function App() {
-  
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) 
+        setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="app">
-      App
+    <div className="app bg-gray-20 ">
+      <Navbar
+        isTopOfPage={isTopOfPage} 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
